76d95c28fc60b24d9efdc7b979fb1a4daca05c6e
IBNBlank <847235539@qq.com>
2019-01-29 18:53:08 +0800

    2019/01/29
    Signed-off-by: IBNBlank <847235539@qq.com>

--- js_turtle_teleop/scripts/js_turtle_teleop_node_py.py
+++ js_turtle_teleop/scripts/js_turtle_teleop_node_py.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python
+
 # -*- coding: utf-8 -*-
 # @Author: IBNBlank
 # @Date:   2019-01-28 22:50:17
 # @Last Modified by:   IBNBlank
-# @Last Modified time: 2019-01-28 22:50:17
+# @Last Modified time: 2019-01-29 18:52:52
