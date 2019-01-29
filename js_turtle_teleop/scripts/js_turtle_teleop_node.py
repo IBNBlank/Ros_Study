371f006553937ad7cdd331cc27627846c09fbd79
IBNBlank <847235539@qq.com>
2019-01-29 21:54:51 +0800

    2019/01/29
    Signed-off-by: IBNBlank <847235539@qq.com>

--- js_turtle_teleop/scripts/js_turtle_teleop_node.py
+++ js_turtle_teleop/scripts/js_turtle_teleop_node.py
@@ -4,28 +4,39 @@
 # @Author: IBNBlank
 # @Date:   2019-01-28 22:50:17
 # @Last Modified by:   IBNBlank
-# @Last Modified time: 2019-01-29 18:52:52
+# @Last Modified time: 2019-01-29 21:54:21
 
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
-class 
-    def callback(data):
-        twist = Twist()
-        twist.linear.x = 4*data.axes[1]
-        twist.angular.z = 4*data.axes[0]
-        pub.publish(twist)
+class Teleop(object):
+    def __init__(self):
+        self.__pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=1)
+        self.__sub = rospy.Subscriber("joy", Joy, self.callback, queue_size=10)
+        self.__max_lin_speed = rospy.get_param('~max_angular_speed', 2.0)
+        self.__max_ang_speed = rospy.get_param('~max_linear_speed', 2.0)
+        self.__lin_speed = 0
+        self.__ang_speed = 0
+        self.__lin_axis = rospy.get_param('~linear_axis_num', 1)
+        self.__ang_axis = rospy.get_param('~angular_axis_num', 3)
 
-    def start():
-        # publishing to "turtle1/cmd_vel" to control turtle1
-        global pub
-        pub = rospy.Publisher('turtle1/cmd_vel', Twist)
-        # subscribed to joystick inputs on topic "joy"
-        rospy.Subscriber("joy", Joy, callback)
-        # starts the node
-        rospy.init_node('Joy2Turtle')
-        rospy.spin()
+    def pub_twist(self):
+        twist = Twist()
+        twist.linear.x = self.__lin_speed
+        twist.angular.z = self.__ang_speed
+        self.__pub.publish(twist)
+
+    def callback(self, data):
+        self.__lin_speed = data.axes[self.__lin_axis] * self.__max_lin_speed
+        self.__ang_speed = data.axes[self.__ang_axis] * self.__max_ang_speed
+        rospy.loginfo("Turtle speed change: linear:{0:.3f} angular:{1:.3f}".format(self.__lin_speed, self.__ang_speed))
 
     if __name__ == '__main__':
-        start()
+        rospy.init_node('js_turtle_teleop_node', anonymous=True)
+        teleop = Teleop()
+        rate = rospy.Rate(50)
+
+        while not rospy.is_shutdown():
+            teleop.pub_twist()
+            rate.sleep()
